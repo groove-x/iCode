f18cf7c8844a903d24d6354b39e849bd6e32c047
takegami <hiroyuki.takegami@groove-x.com>
2019-04-16 16:43:00 +0900

    move common source code to common dir

--- common.cmake
+++ common.cmake
@@ -1,10 +1,3 @@
-set(LIBRFAL_INC
-  ../utils/Inc
-  ../platform/Inc
-  ../rfal/Inc
-  ../rfal/Src/st25r3911
-  ../rfal/Src)
-
 message("build for ${ARCH}")
 if (${ARCH} MATCHES "arm64")
   SET(CMAKE_C_COMPILER aarch64-linux-gnu-gcc)
@@ -12,10 +5,10 @@ if (${ARCH} MATCHES "arm64")
 endif()
 
 include_directories(
-  ../utils/Inc
-  ../platform/Inc
-  ../rfal/Inc
-  ../rfal/Src/st25r3911
-  ../rfal/Src)
+  ../common/utils/Inc
+  ../common/platform/Inc
+  ../common/rfal/Inc
+  ../common/rfal/Src/st25r3911
+  ../common/rfal/Src)
 
 link_directories(../librfal/build/)
